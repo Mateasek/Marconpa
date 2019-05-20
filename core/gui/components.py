46105a9715e0580747d778778b445206027583ee
Matej Tomes <matej.tomes@gmail.com>
2019-05-20 11:00:41 +0200

    Added text area
    Text area for contents of each configuration files added. The init function started to be too large. app.py contents were separated into more files.

---
+++ core/gui/components.py
@@ -0,0 +1,52 @@
+import dash_core_components as dcc
+import dash_html_components as html
+from Marconpa.core.gui.config.layout import (
+    config_layout_channels,
+    config_layout_waveform,
+)
+
+
+def generate_tab_channels(app, config_instance, key):
+
+    textarea = generate_config_textarea(config_instance, key)
+    tab = dcc.Tab(
+        label=key,
+        id=key,
+        children=[
+            config_layout_channels(key, config_instance, app),
+            html.Details(
+                [html.Summary("{0} Configuration File Text".format(key)), textarea]
+            ),
+        ],
+    )
+
+    return tab
+
+
+def generate_tab_waveform(app, config_instance, key):
+
+    textarea = generate_config_textarea(config_instance, key)
+    tab = dcc.Tab(
+        label=key,
+        id=key + ": config,",
+        children=[
+            config_layout_waveform("config-" + key, config_instance, app),
+            html.Details(
+                [html.Summary("{0} Configuration File Text".format(key)), textarea]
+            ),
+        ],
+    )
+    return tab
+
+
+def generate_config_textarea(config_instance, key):
+    textarea = html.Div(
+        dcc.Textarea(
+            placeholder="{0} configuration file".format(key),
+            id="text_{0}".format(key),
+            value=config_instance.as_string(),
+            style={"width": "100%", "height": 600},
+        )
+    )
+
+    return textarea
